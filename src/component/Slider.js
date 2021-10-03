381cab38c0a0d59012055b366cdf1619b175db0d
kuchkorov <kuchkorov.mamurjon@mail.ru>
2021-10-04 00:11:18 +0500

    data.js gacha

---
+++ src/component/Slider.js
@@ -0,0 +1,128 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    // background-color: coral;
+    position:relative;
+    overflow:hidden;
+`
+
+const Arrow = styled.div`
+    width:50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius:50%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    cursor:pointer;
+    position:absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props=> props.direction === "left" && "10px"};
+    right: ${props=> props.direction === "right" && "10px"}; 
+    margin: auto;
+    cursor:pointer;
+    opacity: 0.5;
+    z-index: 2;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+    display:flex;
+    transform: translateX(0vw);
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items:center;
+    background-color: #${props=>props.bg}
+`;
+
+const ImgContainer  = styled.div`
+    flex: 1;
+    height:100%;
+    margin-left: 30px;
+    
+    `
+const Image = styled.img`
+    height: 80%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0;
+    font-size: 20px; 
+    font-weight: 500; 
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer; 
+`
+
+const Slider = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {};
+    return (
+        <Container>
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper>
+                <Slide bg="f5fafd">
+                    <ImgContainer>
+                        <Image src="https://images.pexels.com/photos/5444881/pexels-photo-5444881.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                        <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                </Slide>
+
+                <Slide bg="fcf1ed">
+                    <ImgContainer>
+                        <Image src="https://images.pexels.com/photos/5444881/pexels-photo-5444881.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>WINTER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                        <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                </Slide>
+
+                <Slide bg="fbf0f4">
+                    <ImgContainer>
+                        <Image src="https://images.pexels.com/photos/5444881/pexels-photo-5444881.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>POPULAR SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                        <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
+}
+
+export default Slider;
